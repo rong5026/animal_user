4b96abea8a1465ca0e7894e0dc0504b0ed7a5f3e
rong5026 <rong5026@naver.com>
2023-05-10 14:24:33 +0900

    [ADD] 유저정보usestate로 관리

--- src/ Matching/MatchResult.jsx
+++ src/ Matching/MatchResult.jsx
@@ -8,21 +8,30 @@ function MatchResult() {
 
     const usersCollectionRef = collection(db, "animal-database");
 
+    const [userData, setUserData] = useState([])
+
     useEffect(() => {
 
         const getUsers = async () => {
             // getDocs로 컬렉션안에 데이터 가져오기
             const data = await getDocs(usersCollectionRef);
 
+            //users안 데이터 전부
             data.forEach((doc)=>{
-                console.log(doc.data())
+                const data = doc.data()
+                // console.log(doc.data().phone)
+                const newData = {
+                    name : data.name,
+                    phone : data.phone,
+                    gender : data.gender,
+                }
+                setUserData(userData => [...userData, newData]);
             })
         }
-
-        
-
-
+        getUsers()
     }, [])
+
+
     
     return (
         <div id='matchform'>

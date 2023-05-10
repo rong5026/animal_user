be2fd4fc3dc5f88bb6e34cb02c3e9e5ce4400c70
rong5026 <rong5026@naver.com>
2023-05-10 15:16:33 +0900

    [ADD] 매칭확인 페이지

--- src/ Matching/MatchResult.jsx
+++ src/ Matching/MatchResult.jsx
@@ -3,6 +3,10 @@ import "./MatchResult.css"
 import {addDoc, collection, getDocs} from "firebase/firestore";
 import {useState, useEffect} from 'react';
 import {db} from '../Firebase/FirebaseConfig';
+import animal1 from '../Image/자산 1.png'
+import animal2 from '../Image/자산 2.png'
+
+const userMatchForm = props => (<div></div>);
 
 function MatchResult() {
 
@@ -17,22 +21,23 @@ function MatchResult() {
             const data = await getDocs(usersCollectionRef);
 
             //users안 데이터 전부
-            data.forEach((doc)=>{
+            data.forEach((doc) => {
                 const data = doc.data()
                 // console.log(doc.data().phone)
                 const newData = {
-                    name : data.name,
-                    phone : data.phone,
-                    gender : data.gender,
+                    name: data.name,
+                    phone: data.phone,
+                    gender: data.gender
                 }
-                setUserData(userData => [...userData, newData]);
+                setUserData(userData => [
+                    ...userData,
+                    newData
+                ]);
             })
         }
         getUsers()
     }, [])
 
-
-    
     return (
         <div id='matchform'>
             <div className='match-title'>
@@ -40,10 +45,39 @@ function MatchResult() {
             </div>
             <div className='match-result'>
                 <div className='match-my-gender'>
-                    sdf
+                    {
+                        userData.map((list, index) => {
+                            return (
+                                <div className='match-user-form'>
+                                    <div className='match-icon-container'>
+                                        <img className='match-icon' src={animal1}></img>
+                                    </div>
+                                    <div className='match-animal-name'>
+                                        {list.name}
+                                    </div>
+                                </div>
+
+                            );
+                        })
+                    }
                 </div>
+                <div className='empty-line'></div>
                 <div className='match-opponent-gender'>
-                    df
+                {
+                        userData.map((list, index) => {
+                            return (
+                                <div className='match-user-form'>
+                                    <div className='match-icon-container'>
+                                        <img className='match-icon' src={animal2}></img>
+                                    </div>
+                                    <div className='match-animal-name'>
+                                        {list.name}
+                                    </div>
+                                </div>
+
+                            );
+                        })
+                    }
                 </div>
             </div>
 

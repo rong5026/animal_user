d439d9d5818aba02030e16f91a9a364bd6970067
rong5026 <rong5026@naver.com>
2023-05-18 11:56:55 +0900

    [ADD] 안내사항 추가

--- src/Enter/Enter.jsx
+++ src/Enter/Enter.jsx
@@ -140,7 +140,7 @@ function EnterMain() {
 
                                     <button className='login-button' onClick={onClickLogin}>미팅 참여</button>
                                     <Link className='button-link' to={`/booth`}>
-                                        <button className='login-button'>부스소개</button>
+                                        <button className='login-button'>미팅 안내</button>
                                     </Link>
                                     <Link className='button-link' target='_blank' to={`https://open.kakao.com/o/s3Dz8mkf`}>
                                         <button className='login-button'>문의하기</button>
